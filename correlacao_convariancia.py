3df847e67b5edddab7fd5144ca2f416cbcb38847
Arlindo21 <arlindolima307@gmail.com>
2026-05-04 10:01:18 -0300

    Continuação

--- correlacao_convariancia.py
+++ correlacao_convariancia.py
@@ -30,3 +30,33 @@ print(f'O Desvio de Padrão é {np.std(alturas):.2f}')
 #print(media_alturas)
 # 2ª Forma chamando já no print 
 print(f'A média das alturas  é : {np.mean(alturas)}')
+
+
+#covariencia positiva
+#Duas Coisas crescem ou decrescem ao mesmo tempo 
+
+#negativa
+#quando uma variavel cresce e a outra decresce
+# covariencia zero 
+# nadda muda 
+
+dados_equipamentos ={
+    'Consumo ':[10,20,30,40,50,60,70,80],
+    'tempratura':[25,32,45,48,60,72,85,0]
+
+}
+# Transformçãos dos dados para a leitura em Pandas 
+df_dados = pd.DataFrame(dados_equipamentos)
+
+# fazendo o calculo da covariância
+#criar uma base para a covariança
+cov_matrix =df_dados.cov()
+
+print(f'A covariencia foi :{cov_matrix.iloc[0,1]}')
+
+# correlação
+
+
+print(df_dados.corr())
+
+
